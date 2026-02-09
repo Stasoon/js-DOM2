c90f9e282dde1e8e895d555ba01a48484ba3ea21
Stasoon <kn24-s.paliienko@nubip.edu.ua>
2026-02-09 16:19:35 +0200

    5 foto

--- src/script.js
+++ src/script.js
@@ -8,7 +8,7 @@ const shuffleBtn = document.getElementById("shuffleGallery");
 let page = 1;
 
 // ---- Функція запиту картинок ----
-async function loadImages(count = 4) {
+async function loadImages(count = 5) {
     const response = await fetch(`https://picsum.photos/v2/list?page=${page}&limit=${count}`);
     const data = await response.json();
     page++; // наступна сторінка
